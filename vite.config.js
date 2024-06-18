5da39f039aa1cb922f50e1d24cd1a5c34eb35e81
Leonelborjas <leonelborjas@outlook.es>
2024-06-18 04:30:23 -0300

    cambios

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import path from 'path';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@layouts': path.resolve(__dirname, 'src/layouts'),
-      // Aquí puedes agregar más alias según sea necesario para otras rutas
+      '@layouts': './layouts/Layout', // Ajusta esta ruta según la estructura de tu proyecto
     },
   },
 });
