122b46125d0db99721255ccff60ccf39bf66c5a1
Leonelborjas <leonelborjas@outlook.es>
2024-06-18 12:07:43 -0300

    img

--- src/Components/HeaderLogIn.jsx
+++ src/Components/HeaderLogIn.jsx
@@ -6,7 +6,7 @@ const HeaderLogIn = () => {
         <div className='bg-black gap-8 py-5 flex justify-between items-center px-4 md:px-10 lg:px-20 h-[15vh]'>
             <div className='flex items-center' >
             <picture className='flex gap-2  items-center'>
-                <img className='rounded-full w-8 md:w-16' src="public/assets/img/penguin.jpg" alt="bank logo" />
+                <img className='rounded-full w-8 md:w-16' src="/assets/img/penguin.jpg" alt="bank logo" />
                 <h1 className='text-white font-bold text-1xl md:text-4xl lg:text-5x1'>DHUE BANK</h1>
             </picture>
             </div>
--- src/Components/HeaderNav.jsx
+++ src/Components/HeaderNav.jsx
@@ -4,19 +4,19 @@ import React from 'react';
 
 const HeaderNav = () => {
     const activePage = {
-        Accounts: "public/assets/img/account-active.png",
-        Cards: "public/assets/img/cards-active.png",
-        "DHUE BANK": "public/assets/img/penguin.jpg",
-        Loans: "public/assets/img/loan-active.png",
-        Transactions: "public/assets/img/transaciton-img.png"
+        Accounts: "/assets/img/account-active.png",
+        Cards: "/assets/img/cards-active.png",
+        "DHUE BANK": "/assets/img/penguin.jpg",
+        Loans: "/assets/img/loan-active.png",
+        Transactions: "/assets/img/transaciton-img.png"
     }
 
     const anchors = [
-        { text: "Accounts", to: "/Accounts", img: "/public/assets/img/Accounts.webp", activeIcon: activePage.Accounts },
-        { text: "Cards", to: "/Card", img: "/public/assets/img/cards.webp", activeIcon: activePage.Cards },
-        { text: "DHUE BANK", to: "/Accounts", img: "/public/assets/img/penguin.jpg", activeIcon: activePage["DHUE BANK"], className: "w-15" },
-        { text: "Loans", to: "/Loans", img: "/public/assets/img/loan.webp", activeIcon: activePage.Loans },
-        { text: "Transactions", to: "/Transactions", img: "public/assets/img/transfer.webp", activeIcon: activePage.Transactions }
+        { text: "Accounts", to: "/Accounts", img: "/assets/img/Accounts.webp", activeIcon: activePage.Accounts },
+        { text: "Cards", to: "/Card", img: "/assets/img/cards.webp", activeIcon: activePage.Cards },
+        { text: "DHUE BANK", to: "/Accounts", img: "/assets/img/penguin.jpg", activeIcon: activePage["DHUE BANK"], className: "w-15" },
+        { text: "Loans", to: "/Loans", img: "/assets/img/loan.webp", activeIcon: activePage.Loans },
+        { text: "Transactions", to: "/Transactions", img: "/assets/img/transfer.webp", activeIcon: activePage.Transactions }
     ]
     return (
         <div className='h-26 bg-black lg:flex lg:justify-center lg:items-center lg:flex-col'>
